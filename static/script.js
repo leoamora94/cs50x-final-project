b2243fcf6cbcd70fef19a4c4345d61cda14d703f
Leo Amora <leoamora94@gmail.com>
2023-12-19 09:11:39 -0300

    refactoring

--- static/script.js
+++ static/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function sortlist(id) {
     var lb = document.getElementById(id);
     arrTexts = new Array();
@@ -15,21 +14,3 @@ function sortlist(id) {
     }
 }
 
-=======
-function sortlist(id) {
-    var lb = document.getElementById(id);
-    arrTexts = new Array();
-    
-    for(i=0; i<lb.length; i++) {
-      arrTexts[i] = lb.options[i].text;
-    }
-    
-    arrTexts.sort();
-    
-    for(i=0; i<lb.length; i++) {
-      lb.options[i].text = arrTexts[i];
-      lb.options[i].value = arrTexts[i];
-    }
-}
-
->>>>>>> 0db0c6166abf08ea6e6a4e7798b880dc2933653d
